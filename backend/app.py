ecbae13418d2a75b149679d30bfa25cbb0f2d375
Junaid Siddiqi <junaidasiddiqi@gmail.com>
2025-10-02 19:25:22 -0500

    code for adding and deleting an ingredient

--- backend/app.py
+++ backend/app.py
@@ -36,6 +36,35 @@ def get_ingredient_by_id():
     ingredients = execute_read_query(conn, query)
     return jsonify(ingredients)
 
+# Add new ingredient
+@app.route('/api/ingredient', methods=['POST'])
+def add_ingredient():
+    request_data = request.get_json()
+    name = request_data['ingredientname']
+    amount = request_data['totalamount']
+
+    query = "INSERT INTO ingredient (ingredientname, totalamount) VALUES ('%s', '%s')" % (name, amount)
+
+    myCreds = creds.Creds()
+    conn = create_connection(myCreds.conString, myCreds.userName, myCreds.password, myCreds.dbName)
+    execute_query(conn, query)
+
+    return "Ingredient added successfully"
+
+# Delete ingredient by ID
+@app.route('/api/ingredient', methods=['DELETE'])
+def delete_ingredient():
+    request_data = request.get_json()
+    idToDelete = request_data['id']
+
+    query = "DELETE FROM ingredient WHERE id = %s" % idToDelete
+
+    myCreds = creds.Creds()
+    conn = create_connection(myCreds.conString, myCreds.userName, myCreds.password, myCreds.dbName)
+    execute_query(conn, query)
+
+    return "🗑️ Ingredient deleted successfully"
+
 
 
 app.run()
