dae9795ad77787e585d79f81bd5c1f65bb7d91f4
Junaid Siddiqi <junaidasiddiqi@gmail.com>
2025-10-02 19:09:37 -0500

    rispalys all the ingredients in the sql table

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
 import flask
 import creds
 from sql import create_connection, execute_query, execute_read_query
-from flask import jsonify, request
+from flask import jsonify
+from flask import request
 
 # Setting up the application
 app = flask.Flask(__name__)
@@ -11,3 +12,14 @@ app.config["DEBUG"] = True  # Show errors in browser
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>WELCOME TO OUR RECIPE API!</h1>"
+
+# Display all ingredients 
+@app.route('/api/ingredient/all', methods=['GET'])
+def get_all_ingredients():
+    myCreds = creds.Creds()
+    conn = create_connection(myCreds.conString, myCreds.userName, myCreds.password, myCreds.dbName)
+    query = "SELECT * FROM ingredient"
+    ingredients = execute_read_query(conn, query)
+    return jsonify(ingredients)
+
+app.run()
